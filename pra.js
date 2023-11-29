76b8d611690da68450739d04bc25b3e1751f772a
BananaHow <MA990105@stust.edu.tw>
2023-11-29 17:17:24 +0800

    cool

--- pra.js
+++ pra.js
@@ -1,3 +1,25 @@
 const testP = document.getElementById("textP")
 
 testP.innerText = "超酷的啦"
+
+// storage
+// localStorage / sessionStorage
+
+// JSON.Stringify
+// JSON.parse
+
+// let dog = {
+//     name:"可口可樂",
+//     type:"臘腸",
+//     age:2
+// }
+
+// let dog = [1,2,3,4,5]
+let dog = 11
+
+localStorage.setItem("dog", JSON.stringify(dog))
+console.log(JSON.stringify(dog))
+console.log(typeof(JSON.stringify(dog)))
+
+let obj = localStorage.getItem("dog")
+console.log(JSON.parse(obj))
